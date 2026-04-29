897352857e2d6f607716f4d8073fb8c0723da102
thodalf <thodalf@gmail.com>
2026-04-29 20:45:13 +0200

    Import Firebase Database functions
    Added Firebase Database functions for data manipulation.

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { 
+  getDatabase, 
+  ref, 
+  set, 
+  get, 
+  onValue, 
+  update, 
+  remove,
+  child 
+} from 'firebase/database';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
